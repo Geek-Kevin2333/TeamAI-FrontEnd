f2f127c024f02120146c4ba7bad5b1890be4ad88
Geek-Kevin2333 <q1835894540@163.com>
2022-02-05 14:09:06 +0800

    2.5

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,22 @@
+import axios from 'axios';
 import request from '../utils/request';
 
 export const fetchData = query => {
     return request({
-        url: './table.json',
+        url: './simpleConfig.json',
         method: 'get',
         params: query
     });
 };
+// export const fetchData = (userId)=>{
+//     // axios.get('/api/config/get/all_new',{
+//        axios.get('./simple_Config.json',{
+//             params:{
+//                 userId
+//             }
+//         }).
+//         then(function (response) {
+  
+//             return response
+//         })
+// }
--- src/views/BaseTable.vue
+++ src/views/BaseTable.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <!-- 历史记录，面包屑 -->
         <div class="crumbs">
             <el-breadcrumb separator="/">
                 <el-breadcrumb-item>
@@ -7,73 +8,49 @@
                 </el-breadcrumb-item>
             </el-breadcrumb>
         </div>
-        <div class="container">
-            <div class="handle-box">
-                <el-select v-model="query.address" placeholder="地址" class="handle-select mr10">
-                    <el-option key="1" label="广东省" value="广东省"></el-option>
-                    <el-option key="2" label="湖南省" value="湖南省"></el-option>
-                </el-select>
-                <el-input v-model="query.name" placeholder="用户名" class="handle-input mr10"></el-input>
-                <el-button type="primary" icon="el-icon-search" @click="handleSearch">搜索</el-button>
-            </div>
-            <el-table :data="tableData" border class="table" ref="multipleTable" header-cell-class-name="table-header">
-                <el-table-column prop="id" label="ID" width="55" align="center"></el-table-column>
-                <el-table-column prop="name" label="用户名"></el-table-column>
-                <el-table-column label="账户余额">
-                    <template #default="scope">￥{{ scope.row.money }}</template>
-                </el-table-column>
-                <el-table-column label="头像(查看大图)" align="center">
-                    <template #default="scope">
-                        <el-image class="table-td-thumb" :src="scope.row.thumb" :preview-src-list="[scope.row.thumb]">
-                        </el-image>
-                    </template>
-                </el-table-column>
-                <el-table-column prop="address" label="地址"></el-table-column>
-                <el-table-column label="状态" align="center">
-                    <template #default="scope">
-                        <el-tag :type="
-                                scope.row.state === '成功'
-                                    ? 'success'
-                                    : scope.row.state === '失败'
-                                    ? 'danger'
-                                    : ''
-                            ">{{ scope.row.state }}</el-tag>
-                    </template>
-                </el-table-column>
 
-                <el-table-column prop="date" label="注册时间"></el-table-column>
-                <el-table-column label="操作" width="180" align="center">
-                    <template #default="scope">
-                        <el-button type="text" icon="el-icon-edit" @click="handleEdit(scope.$index, scope.row)">编辑
-                        </el-button>
-                        <el-button type="text" icon="el-icon-delete" class="red"
-                            @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-                    </template>
-                </el-table-column>
-            </el-table>
+    <div class="container">
+    
+    <!-- 详情页 -->
+    <div>
+    <el-dialog v-model="dialogTableVisible" title="配置详情">
+    <el-table :data="gridData">
+      <el-table-column
+        property="date"
+        label="Date"
+        width="150"
+      ></el-table-column>
+      <el-table-column
+        property="name"
+        label="Name"
+        width="200"
+      ></el-table-column>
+      <el-table-column property="address" label="Address"></el-table-column>
+     </el-table>
+   </el-dialog>
+   </div>
+
+    <div class="mytTable">
+         <el-table :data="tableData" border style="width: 100%">
+         <el-table-column prop="fileId" label="文件ID" width="180" />
+         <el-table-column prop="fileName" label="文件名" width="180" />
+         <el-table-column prop="configId" label="配置ID" width="180" />
+         <el-table-column prop="confName" label="配置名" />
+         
+         <el-table-column  label="Operations">
+             <template #default="scope" >
+                 <el-button type="text" size="small" @click="handleDetail(scope.row)">详情</el-button>
+             </template>
+        </el-table-column>
+        
+        </el-table>
+    </div>
             <div class="pagination">
                 <el-pagination background layout="total, prev, pager, next" :current-page="query.pageIndex"
                     :page-size="query.pageSize" :total="pageTotal" @current-change="handlePageChange"></el-pagination>
             </div>
-        </div>
+    </div>
 
-        <!-- 编辑弹出框 -->
-        <el-dialog title="编辑" v-model="editVisible" width="30%">
-            <el-form label-width="70px">
-                <el-form-item label="用户名">
-                    <el-input v-model="form.name"></el-input>
-                </el-form-item>
-                <el-form-item label="地址">
-                    <el-input v-model="form.address"></el-input>
-                </el-form-item>
-            </el-form>
-            <template #footer>
-                <span class="dialog-footer">
-                    <el-button @click="editVisible = false">取 消</el-button>
-                    <el-button type="primary" @click="saveEdit">确 定</el-button>
-                </span>
-            </template>
-        </el-dialog>
     </div>
 </template>
 
@@ -93,73 +70,44 @@ export default {
         });
         const tableData = ref([]);
         const pageTotal = ref(0);
+        const userId=ref(1);
+        var  dialogTableVisible = ref(false)
         // 获取表格数据
--     [x] //插入获取数据的api，可以引入第三方文件
+        //  插入获取数据的api，可以引入第三方文件
         const getData = () => {
-            fetchData(query).then((res) => {
+            console.log("getData被调用了")
+            fetchData(userId)
+            .then((res) => {
                 tableData.value = res.list;
                 pageTotal.value = res.pageTotal || 50;
             });
         };
+
+        // 相当于beforeCreated生命周期中调用了getData（）函数
         getData();
 
-        // 查询操作
-        const handleSearch = () => {
-            query.pageIndex = 1;
-            getData();
-        };
+       
         // 分页导航
         const handlePageChange = (val) => {
-            query.pageIndex = val;
+            // query.pageIndex = val;
             getData();
         };
 
-        // 删除操作
-        const handleDelete = (index) => {
-            // 二次确认删除
-            ElMessageBox.confirm("确定要删除吗？", "提示", {
-                type: "warning",
-            })
-                .then(() => {
-                    ElMessage.success("删除成功");
-                    tableData.value.splice(index, 1);
-                })
-                .catch(() => {});
-        };
+        const handleDetail=(val)=>{
+            dialogTableVisible=true
+            console.log(val)
+        }
 
-        // 表格编辑时弹窗和保存
-        const editVisible = ref(false);
-        let form = reactive({
-            name: "",
-            address: "",
-        });
-        let idx = -1;
-        const handleEdit = (index, row) => {
-            idx = index;
-            Object.keys(form).forEach((item) => {
-                form[item] = row[item];
-            });
-            editVisible.value = true;
-        };
-        const saveEdit = () => {
-            editVisible.value = false;
-            ElMessage.success(`修改第 ${idx + 1} 行成功`);
-            Object.keys(form).forEach((item) => {
-                tableData.value[idx][item] = form[item];
-            });
-        };
+        
 
         return {
             query,
             tableData,
             pageTotal,
-            editVisible,
-            form,
-            handleSearch,
+            userId,
             handlePageChange,
-            handleDelete,
-            handleEdit,
-            saveEdit,
+            handleDetail,
+            dialogTableVisible,
         };
     },
 };
